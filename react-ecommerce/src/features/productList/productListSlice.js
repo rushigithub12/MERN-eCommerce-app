1e12b468c901943976a8f049edbea3a5d5c9ac7c
rushigithub12 <rushikeshkamble9256@gmail.com>
2024-07-21 20:40:54 +0530

    initilal commit

---
+++ react-ecommerce/src/features/productList/productListSlice.js
@@ -0,0 +1,34 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchProductList } from "./productListAPI";
+
+const initialState = {
+  value: 0,
+  status: "idle",
+};
+
+export const incrementAsync = createAsyncThunk(
+  "counter/fetchProductList",
+  async (amount) => {
+    const response = await fetchProductList(amount);
+    return response.data;
+  }
+);
+
+export  const productListSlice = createSlice({
+  name: "productList",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+  },
+  // extraReducers: (builder) => {
+  //   () => {}
+  // },
+});
+
+export const { increment } = productListSlice.actions;
+
+export const selectProductList = (state) => state.productList;
+
+export default productListSlice.reducer;
