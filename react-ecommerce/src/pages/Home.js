1326e6d48b6f9ec0d028c95ff363fe0dc2d27c82
rushigithub12 <rushikeshkamble9256@gmail.com>
2024-10-31 15:17:11 +0530

    remove console from home component

--- react-ecommerce/src/pages/Home.js
+++ react-ecommerce/src/pages/Home.js
@@ -1,18 +1,15 @@
-import React from 'react'
-import Navbar from '../features/navbar/Navbar'
-import ProductList from '../features/productList/components/ProductList'
+import React from "react";
+import Navbar from "../features/navbar/Navbar";
+import ProductList from "../features/productList/components/ProductList";
 
 function Home() {
-   
-   console.log("this is HOME component==>>");
-
   return (
-      <>
-         <Navbar>
-            <ProductList />
-         </Navbar>
-      </>
-  )
+    <>
+      <Navbar>
+        <ProductList />
+      </Navbar>
+    </>
+  );
 }
 
-export default Home
+export default Home;
