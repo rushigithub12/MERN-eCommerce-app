113152300b5094f0106022da442d475546d2f18b
rushigithub12 <82263330+rushigithub12@users.noreply.github.com>
2025-12-21 12:43:18 +0530

    correct backend URL (#5)

--- react-ecommerce/src/features/order/orderAPI.js
+++ react-ecommerce/src/features/order/orderAPI.js
@@ -40,36 +40,3 @@ export async function updateOrder(order) {
       throw new Error(error.message || "Failed to update order");
     });
 }
-
-export function fetchAllOrders(sort, pagination) {
-  let queryString = "";
-  for (let key in pagination) {
-    queryString += `${key}=${pagination[key]}&`;
-  }
-
-  for (let key in sort) {
-    queryString += `${key}=${sort[key]}&`;
-  }
-
-  return new Promise(async (resolve) => {
-    const response = await fetch("http://localhost:8080/orders?" + queryString);
-    const ordersdData = await response.json();
-    resolve({
-      data: { orders: ordersdData.data, totalOrders: ordersdData.items },
-    });
-  });
-}
-
-export function updateOrder(order) {
-  return new Promise(async (resolve) => {
-    const response = await fetch("http://localhost:8080/orders/" + order.id, {
-      method: "PATCH",
-      body: JSON.stringify(order),
-      headers: {
-        "content-type": "application/json",
-      },
-    });
-    const data = await response.json();
-    resolve({ data });
-  });
-}
